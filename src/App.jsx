2ab2c04928e9a64c3bcd659f24850b34f3c57cd6
unknown <aliopdandan@gmail.com>
2023-10-18 21:02:43 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+    <form id="form">
+      <input type="text" name="todoInput" id="todoInput"/>
+      <button>addtodo</button>
+    </form>
+    <div id="todo-list">
+    <div>
+      <input type="checkbox"/>
+      <label>Todo</label>
+    </div>
+    <div>
+      <input type="checkbox"/>
+      <label>Todo</label>
+    </div>
+    </div>
+    </>
+  )
+                            
+}
+
+export default App
